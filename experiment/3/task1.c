2d7d54e52e8988a28af80e5270b1d90a48c7cd61
seamoonsbird <ABC19849650486@163.com>
2025-12-03 09:51:37 +0800

    3task1 1.0.0

---
+++ experiment/3/task1.c
@@ -0,0 +1,69 @@
+/*请用C语言实现：铁路购票系统座位分配算法，用于处理一节车厢的座位分配。
+规则如下：假设一节车厢有20排，每一排有5个座位，用A、B、C、D、F表示，
+第一排就是1A、1B、1C、1E、1F，第二排就是2A、2B、2C、2D、2F，以此类推，
+购票时，每次最多购买5张，座位分配策略师：
+如果这几张票能安排在同一排相邻的座位，则安排在编号最小的相邻座位，
+否则，安排在编号最小的几个空座位中（不考虑是否相邻）
+*/
+
+#include <stdio.h>
+#define N 5
+#define M 20
+int main(void){
+    int m;
+    printf("请问你要买几次票\n");
+    scanf("%d",&m);
+    if (m>100){
+        printf("票数不够\n");
+        return 0;
+    }
+    int seat[M][N]={0};
+    int i=0;
+    int j=0;
+    printf("请问你要分别买几张票\n");
+    int n[M]={0};
+    for (i=0;i<m;i++){
+        scanf("%d",n+i);
+    }
+    for (int k=0;k<m;k++){
+        if (n[k]>5){
+            printf("超出5张票不卖\n");
+            return 0;
+        }
+    }
+    int k=0;
+    for (k=0;k<m;k++){
+        for(i=0;i<M;i++){
+
+            j=0;
+            while(seat[i][j]!=0){
+                if(++j>N){
+                    break;
+                }
+            }
+            if(seat[i][j]==0&&j+n[k]<=N){
+                for(int l=0;l<n[k];l++){
+                    seat[i][j]=1;
+                    printf("\t%d%c",i+1,j+65);
+                    j++;
+                }
+                printf("\n");
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+        if(i==M){
+            for(int l=0;l<M*N;l++){
+                int *p=&seat[0][0];
+                if(*(p+l)==0){
+                    printf("\t%d%c",l/5+1,l%5+65);
+                    *(p+l)=1;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
