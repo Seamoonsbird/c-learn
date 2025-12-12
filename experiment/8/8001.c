cf64caa05357edcdc8c14cec76cb3513c647b5a7
seamoonsbird <ABC19849650486@163.com>
2025-12-12 09:03:56 +0800

    8001 1.0

---
+++ experiment/8/8001.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// 函数原型声明(函数的具体实现需要你在文件末尾完成)
+void sort_interest(int *interest, int n);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int interest[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &interest[i]);
+    }
+    sort_interest(interest, n);
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            printf(" ");
+        }
+        printf("%d", interest[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+// 你需要实现的函数
+void sort_interest(int *interest, int n) {
+    // 请在此处编写代码
+    //选择排序
+    for (int i=0;i<n-1;i++){
+        int k=i;
+        for (int j=i+1;j<n;j++){
+            if (interest[j]>interest[k]){
+                k=j;
+            }
+        }
+        if (k!=i){
+            int temp=interest[k];
+            interest[k]=temp;
+            interest[i]=interest[k];
+        }
+        
+    }
+}
