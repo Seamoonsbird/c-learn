551d41ca9489fe9609d79c1582f3f8f6a1089ced
seamoonsbird <ABC19849650486@163.com>
2025-12-14 20:09:05 +0800

    8002 3.0 finish

--- experiment/8/8002.c
+++ experiment/8/8002.c
@@ -36,26 +36,7 @@ void find_top_k_index(int *interest, int n, int k, int *top_k_index) {
     // 其长度固定为 100，但实际上只需将返回结果写入到其前 k 个元素即可
 
     // 请在此处编写代码
-    //如果用堆来算，难点在于不是让你排序，而是让你输出角标，一但建了堆，就会出现角标混乱
-    //能不能用指针建堆
-    //建小顶堆
-/*
-    int *temp_arr[100];
-    for (int i=0;i<k;i++){
-        temp_arr[i]=&interest[i];
-    }
-    for (int i=0;i<k;i++){
-        top_k_index[i]=i;
-    }
-    for (int i=k/2-1;k>=0;k--){
-        heapAdjust(temp_arr,k,i);
-    }
-    //测试是否建好堆
-    for (int i=0;i<k;i++){
-        printf("%d",*temp_arr[i]);
-    }
-    //传入剩下元素
-*/
+    
     //建立临时堆并进行初始化
     heap temp_arr[100];
     for (int i=0;i<k;i++){
@@ -76,34 +57,69 @@ void find_top_k_index(int *interest, int n, int k, int *top_k_index) {
    
     //建立小顶堆
     for (int i=k/2-1;i>=0;i--){
-        heapAdjust(temp_arr,n,i);
+        heapAdjust(temp_arr,k,i);
     }
 
-    /*测试建立小顶堆是否成功*/
+    /*测试建立小顶堆是否成功
     for (int i=0;i<k;i++){
         printf("第%d个数据为%d,他原来的序号是%d\n",i,temp_arr[i].data,temp_arr[i].index);
     }
+    */
+
+    //对剩下元素进行检查，如果小于堆顶就入堆
+    for (int i=k;i<n;i++){
+        if(interest[i]<temp_arr[0].data){
+            continue;
+        }
+        initheap(temp_arr);
+        temp_arr[0].data=interest[i];
+        temp_arr[0].index=i;
+        heapAdjust(temp_arr,k,0);
+    }
+
+    /*测试入堆是否成功
+    for (int i=0;i<k;i++){
+        printf("第%d个数据为%d,他原来的序号是%d\n",i,temp_arr[i].data,temp_arr[i].index);
+    }
+    */
     
+    //对临时堆进行排序
+    for (int i=k-1;i>0;i--){
+        heap temp=temp_arr[0];
+        temp_arr[0]=temp_arr[i];
+        temp_arr[i]=temp;
+        heapAdjust(temp_arr,i,0);//复制粘贴错了，写了heapAdjust(temp_arr,k,0)
+    }
 
+    /*测试堆排序是否成功
+    for (int i=0;i<k;i++){
+        printf("第%d个数据为%d,他原来的序号是%d\n",i,temp_arr[i].data,temp_arr[i].index);
+    }
+    */
 
+    ///将临时堆中的index赋给top_k_index
+    for (int i=0;i<k;i++){
+        *(top_k_index+i)=temp_arr[i].index;
+    }
+    
 }
-void heapAdjust(heap arr[],int n,int i){
+void heapAdjust(heap arr[],int k,int i){
     int maxidx=i;
     int left=2*i+1;
     int right=2*i+2;
-    if (left<n&&arr[maxidx].data>arr[left].data){
+    if (left<k&&arr[maxidx].data>arr[left].data){
         maxidx=left;
     }
-    if (right<n&&arr[maxidx].data>arr[right].data){
+    if (right<k&&arr[maxidx].data>arr[right].data){
         maxidx=right;
     }
     if (maxidx!=i){
         heap temp=arr[i];
         arr[i]=arr[maxidx];
         arr[maxidx]=temp;
-        heapAdjust(arr,n,maxidx);
+        heapAdjust(arr,k,maxidx);
     }
-}
+}//heapAdjust(arr,n,maxidx)传了n进去，导致数组越界，Windows环境运行正常但是Linux暴露问题
 
 void initheap(heap *arr){
     arr->data=0;
