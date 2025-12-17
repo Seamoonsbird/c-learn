898e09c8807de99b0d86e1e3810a7988c3a6f782
seamoonsbird <ABC19849650486@163.com>
2025-12-17 10:06:02 +0800

    9001test 2.0

--- experiment/9/9001test.c
+++ experiment/9/9001test.c
@@ -47,7 +47,7 @@ StackResult stackPush(Stack* stack, char bracket, int position){
         // 学生实现：
     // 步骤1：检查栈是否满（top+1 == capacity），若满则尝试扩容（需不超过max_capacity）
     if (stack->top+1==stack->capacity&&stack->capacity<=stack->max_capacity){
-        stack->data=(StackElem *)realloc(stack->data,sizeof(StackElem)*(stack->capacity)+1);
+        stack->data=(StackElem *)realloc(stack->data,sizeof(StackElem)*(stack->capacity+1));
     }
     // 步骤2：扩容失败返回STACK_FULL，成功则更新capacity
     if (stack->data==NULL){
@@ -261,3 +261,6 @@ int main(){
     // 返回JSON括号匹配结果，基础版只能处理简单情况，进阶版总能返回正确答案
     printf("%d %d", result1,result2);
 }
+
+
+
