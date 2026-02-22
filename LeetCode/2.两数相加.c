19a682bbbf2d6c3654b1790f3e327af6b633280e
seamoonsbird <ABC19849650486@163.com>
2026-02-22 21:23:03 +0800

    大错特错

--- LeetCode/2.两数相加.c
+++ LeetCode/2.两数相加.c
@@ -35,7 +35,55 @@ struct ListNode *reverse(struct ListNode* l0){
     return head;
 }
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    
+    struct ListNode *h1 = reverse(l1);
+    struct ListNode *h2 = reverse(l2);
+    struct ListNode *c1 = h1;
+    struct ListNode *c2 = h2;
+    struct ListNode *result=NULL;
+    struct ListNode *result_current=result;
+    int remain = 0;
+    while (c1!==NULL&&c2==NULL){
+        struct ListNode newnode = (struct ListNode)malloc(sizeof(struct ListNode));
+        newnode.next = NULL;
+        newnode.val=(*(c1.val)+*(c2.val)+remain)%10;
+        remain = (*(c1.val)+*(c2.val)+remain)/10;
+        if (result==NULL){
+            result=newnode;
+        }else{
+            result_current.next=newnode;
+        }
+        result_current=result_current.next;
+        c1=c1.next;
+        c2=c2.next;
+    }
+    if (c1==NULL){
+        struct ListNode newnode = (struct ListNode)malloc(sizeof(struct ListNode));
+        newnode.next = NULL;
+        newnode.val=(*(c2.val)+remain)%10;
+        remain = (*(c2.val)+remain)/10;
+        result_current.next=newnode;
+        result_current = result_current.next;
+        c2=c2.next;
+    }else if(c2==NULL){
+        struct ListNode newnode = (struct ListNode)malloc(sizeof(struct ListNode));
+        newnode.next = NULL;
+        newnode.val=(*(c1.val)+remain)%10;
+        remain = (*(c1.val)+remain)/10;
+        result_current.next=newnode;
+        result_current = result_current.next;
+        c1=c1.next;
+    }else{
+        if(remain==0){
+
+        }else{
+            struct ListNode newnode = (struct ListNode)malloc(sizeof(struct ListNode));
+            newnode.next = NULL;
+            newnode.val = remain;
+            result_current.next=newnode;
+        }
+    }
+
+    return result;
 }
 // @lc code=end
 
